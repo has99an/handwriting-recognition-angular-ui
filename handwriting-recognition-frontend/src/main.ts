d747ab64475b787cf2d414dcdca45ab33b213993
Melih Øzata <129662619+Melihoz98@users.noreply.github.com>
2024-10-17 10:37:11 +0200

    reg site

--- handwriting-recognition-frontend/src/main.ts
+++ handwriting-recognition-frontend/src/main.ts
@@ -3,8 +3,13 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 
+const { providers: appConfigProviders, ...restAppConfig } = appConfig;  // Destructure appConfig to separate providers
+
 bootstrapApplication(AppComponent, {
-  ...appConfig,
-  providers: [...appConfig.providers, provideHttpClient()]
+  providers: [
+    ...(appConfigProviders || []),  // Use appConfig providers if they exist
+    provideHttpClient(),            // Add additional providers
+  ],
+  ...restAppConfig  // Spread the rest of the appConfig, excluding providers
 })
-  .catch((err) => console.error(err));
+.catch((err) => console.error(err));
